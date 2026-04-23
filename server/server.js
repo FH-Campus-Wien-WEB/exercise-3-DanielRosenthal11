8482bd698b6b34eee320e25835777262abe77cd8
DanielRosenthal <daniel.rosenthal@stud.hcw.ac.at>
2026-04-23 13:36:58 +0200

    feat: part2 - apply CSS Grid layout and verify genre filtering

--- server/server.js
+++ server/server.js
@@ -11,10 +11,7 @@ app.use(bodyParser.json());
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
-/* Task 1.2: Add a GET /genres endpoint:
-   This endpoint returns a sorted array of all the genres of the movies
-   that are currently in the movie model.
-*/
+/* Task 1.2: Add a GET /genres endpoint */
 app.get('/genres', function (req, res) {
   const genres = new Set();
   Object.values(movieModel).forEach(movie => {
@@ -25,10 +22,7 @@ app.get('/genres', function (req, res) {
   res.send(Array.from(genres).sort());
 });
 
-/* Task 1.4: Extend the GET /movies endpoint:
-   When a query parameter for a specific genre is given, 
-   return only movies that have the given genre
- */
+/* Task 2.2: Extend the GET /movies endpoint with query parameters */
 app.get('/movies', function (req, res) {
   let movies = Object.values(movieModel);
   if (req.query.genre) {
